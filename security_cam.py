f650bae699d61def61308dcc29483350990f2140
Ben Rousch <brousch@gmail.com>
2014-10-13 23:13:37 -0400

    First try for security cam

--- security_cam.py
+++ security_cam.py
@@ -1,19 +1,50 @@
-import picamera
+import datetime
+import os
+import time
 
-if __name__ == "__main__":
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1280, 720)
-        camera.framerate = 30
-        camera.start_preview()
-        camera.start_recording('securitycam/highres.h264')
-        camera.start_recording('securitycam/lowres.h264',
-                               splitter_port=2,
-                               resize=(160, 90)) 
-        camera.capture('securitycam/ss-00.jpg', use_video_port=True)
-        camera.wait_recording(5)
-        camera.capture('securitycam/ss-05.jpg', use_video_port=True)
-        camera.wait_recording(5)
-        camera.capture('securitycam/ss-10.jpg', use_video_port=True)
-        camera.stop_recording(splitter_port=2)
-        camera.stop_recording()
-        camera.stop_preview()
+from SimpleCV import Camera
+import RPi.GPIO as GPIO
+
+def get_file_name(extension='png'):
+    dt = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+    return os.path.join("pictures", 'sc-'+dt+'.'+extension)
+
+cam_size = {'width': 640, 'height': 480}
+vid_fps = 25
+
+sensorPin = 7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+prevState = False
+currState = False
+
+print("Initializing camera")
+cam = Camera(0, cam_size)
+r = g = b = 0
+while r + g + b < 0.01:
+    img = cam.getImage()
+    r, g, b = img[img.width/2, img.height/2]
+    print("r: {} g: {} b: {}".format(r,g,b))
+    time.sleep(1)
+
+while True:
+    currState = GPIO.input(sensorPin)
+    time.sleep(.1)
+    if currState:  # True if movement, False if no movement
+        # Save still image
+        file_name = get_file_name('png')
+        print("Movement detected. Saving image {}".format(file_name))
+        img = cam.getImage().save(file_name)
+
+        # Save video
+        file_name = get_file_name('avi')
+        print("Recording video {}".format(file_name))
+        vid = VideoStream(file_name, fps=vid_fps)
+        for frame in range(0, vid_fps * 10):
+            img = cam.getImage()
+            vid.writeFrame(img)
+            time.sleep(1.0/vid_fps)
+    time.sleep(5)
+

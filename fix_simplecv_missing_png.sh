3df3364d4010b5bd846af6f31e4164954aa415b0
Ben Rousch <brousch@gmail.com>
2014-10-28 13:46:21 -0400

    SimpleCV fix script now gets all sample images

--- fix_simplecv_missing_png.sh
+++ fix_simplecv_missing_png.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-wget http://tutorial.simplecv.org/en/latest/_images/simplecv-logo.png
-sudo mkdir /usr/lib/pymodules/python2.7/SimpleCV/sampleimages
-sudo mv ./simplecv-logo.png /usr/lib/pymodules/python2.7/SimpleCV/sampleimages/simplecv.png
-
+wget https://dl.dropboxusercontent.com/u/101667/simplecv-sampleimages.tar
+tar -xf simplecv-sampleimages.tar
+sudo mv simplecv-sampleimages/sampleimages /usr/lib/pymodules/python2.7/SimpleCV/
